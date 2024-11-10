cfdebec114f53c5d8c9aab51f42bc79fea70d46e
Soliken <155744710+soliken1@users.noreply.github.com>
2024-11-10 18:49:53 +0800

    Dashboard Initial Design

---
+++ Controllers/Courses/CoursesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TMS.Controllers.Courses
+{
+    public class CoursesController : Controller
+    {
+        public IActionResult Course()
+        {
+            return View();
+        }
+    }
+}
