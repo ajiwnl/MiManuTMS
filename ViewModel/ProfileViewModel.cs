a2319c1585b56b2cf569668db60db01a3a8cd1ed
ajiwnl <alvinjohnaranetz@gmail.com>
2024-12-10 13:18:47 +0800

    added profile view

---
+++ ViewModel/ProfileViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace TMS.ViewModels
+{
+    public class ProfileViewModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string UserRole { get; set; }
+        public string UserImg { get; set; }
+    }
+}
