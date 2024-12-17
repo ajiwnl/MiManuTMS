e9385553b7b554ffb9a88441c9520e0297f33ca9
soliken <kennethrex456@gmail.com>
2024-12-17 21:49:53 +0800

    Employee Course Screen + Working Calendar

--- Controllers/Courses/CoursesController.cs
+++ Controllers/Courses/CoursesController.cs
@@ -4,9 +4,35 @@ namespace TMS.Controllers.Courses
 {
     public class CoursesController : Controller
     {
-        public IActionResult Course()
+        public IActionResult Course(int? month, int? year)
         {
-            return View();
+            var userRole = HttpContext.Session.GetString("UserRole");
+            ViewData["ActivePage"] = "Courses";
+
+            Calendar(month, year);
+
+            if (userRole == "Employee")
+            {
+                return View("Employee");
+            }
+            else if (userRole == "Trainor")
+            {
+                return View("Trainor");
+            }
+
+            TempData["NotLoggedInMsg"] = "Please Try Logging In!";
+            return RedirectToAction("Error", "Status");
+        }
+
+        public void Calendar(int? month, int? year)
+        {
+            var currentDate = DateTime.Now;
+            int currentMonth = month ?? currentDate.Month;
+            int currentYear = year ?? currentDate.Year;
+
+            // Pass month and year to the view
+            ViewBag.CurrentMonth = currentMonth;
+            ViewBag.CurrentYear = currentYear;
         }
     }
 }
