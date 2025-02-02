40ec199b6b21727799efd965c19a0aa0e24eb036
ajiwnl <alvinjohnaranetz@gmail.com>
2025-02-02 21:30:47 +0800

    profile modules done

--- ViewModel/EditProfileViewModel.cs
+++ ViewModel/EditProfileViewModel.cs
@@ -10,16 +10,19 @@ namespace TMS.ViewModels
 
         public string CurrentEmail { get; set; }
 
-        [Required]
         [DataType(DataType.Password)]
         public string CurrentPassword { get; set; }
 
+        [Required(ErrorMessage = "Old password is required.")]
         [DataType(DataType.Password)]
         public string OldPassword { get; set; }
 
+        [Required(ErrorMessage = "New password is required.")]
         [DataType(DataType.Password)]
+        [MinLength(6, ErrorMessage = "New password must be at least 6 characters long.")]
         public string NewPassword { get; set; }
 
+        [Required(ErrorMessage = "Please confirm your new password.")]
         [DataType(DataType.Password)]
         [Compare("NewPassword", ErrorMessage = "Passwords do not match.")]
         public string ConfirmPassword { get; set; }
@@ -29,4 +32,5 @@ namespace TMS.ViewModels
         public string UserImgUrl { get; set; }
 
     }
+
 }
